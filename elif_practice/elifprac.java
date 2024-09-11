03645922819553d3db2bd3d9b91466e7db11004d
CyberSphinxxx <johnlemargonzales@gmail.com>
2024-09-11 12:12:11 +0800

    minor adjustments

---
+++ elif_practice/elifprac.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class elifprac {
+
+    public static void separator() {
+        System.out.println("---------------------------------");
+    }
+
+    public static void main(String[] args) {
+        Scanner age_input = new Scanner(System.in);
+
+        while (true){
+        separator();
+        System.out.print("Enter age: ");
+        int age = age_input.nextInt();
+
+            if (age < 18) {
+                separator();
+                System.out.println("You are a minor. You cannot enter!");
+            }
+        
+            else if (age >= 18) {
+                separator();
+                System.out.println("You can enter");
+            }
+
+            else {
+                separator();
+                System.out.println("Invalid input.");
+            }
+        }
+    }
+}
